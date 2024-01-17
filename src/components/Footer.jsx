c90f7a0dd75f00223e83116fa425d9a2f1117381
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-01-17 16:55:42 +0100

    Developing Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from "react"
+import {
+  FaPinterestSquare,
+  FaInstagramSquare,
+  FaFacebookSquare,
+} from "react-icons/fa"
+
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t border-gray-100 py-4 text-center">
+      <div className="flex flex-col-reverse items-center justify-center space-x-4 sm:flex-row sm:justify-evenly">
+        <p className="text-sm font-medium text-gray-500 xl:text-lg">
+          &copy; {new Date().getFullYear()} Bargain. All rights reserved.
+        </p>
+        <div className="flex flex-row items-center justify-center space-x-5 py-4 ">
+          <Link
+            to="https://https://pl.pinterest.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaPinterestSquare className="h-[28px] w-[28px] text-gray-600 hover:hover:text-gray-800 xl:h-[32px] xl:w-[32px] " />
+          </Link>
+          <Link
+            to="https://www.instagram.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaInstagramSquare className="h-[28px] w-[28px] text-gray-600 hover:hover:text-gray-800 xl:h-[32px] xl:w-[32px] " />
+          </Link>
+          <Link
+            to="https://www.facebook.com/?locale=pl_PL"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaFacebookSquare className="h-[28px] w-[28px] text-gray-600 hover:hover:text-gray-800 xl:h-[32px] xl:w-[32px] " />
+          </Link>
+        </div>
+      </div>
+    </footer>
   )
 }
 
