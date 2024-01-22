08e06e39a760beb5ac8abfee2f774347d2150d8e
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-01-22 15:54:56 +0100

    Developing smartphone data

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,95 @@
 import logo from "./logo/logo.svg"
+import HuaweiMate50Pro from "./smartphones/Huawei/Huawei Mate 50 Pro/HUAWEI Mate 50 Pro.webp"
+import HuaweiMate50Pro1 from "./smartphones/Huawei/Huawei Mate 50 Pro/HUAWEI Mate 50 Pro B.webp"
+import HuaweiMate50Pro2 from "./smartphones/Huawei/Huawei Mate 50 Pro/HUAWEI Mate 50 Pro FB.webp"
+import HuaweiNova11Pro from "./smartphones/Huawei/Huawei Nova 11/HUAWEI Nova 11 Pro.webp"
+import HuaweiNova11Pro1 from "./smartphones/Huawei/Huawei Nova 11/HUAWEI Nova 11 Pro B.webp"
+import HuaweiNova11Pro2 from "./smartphones/Huawei/Huawei Nova 11/HUAWEI Nova 11 Pro FB.webp"
+import HuaweiP60Pro from "./smartphones/Huawei/Huawei P60 Pro/HUAWEI P60 Pro.webp"
+import HuaweiP60Pro1 from "./smartphones/Huawei/Huawei P60 Pro/HUAWEI P60 Pro B.webp"
+import HuaweiP60Pro2 from "./smartphones/Huawei/Huawei P60 Pro/HUAWEI P60 Pro FB.webp"
+import iPhone15ProMax from "./smartphones/iPhone/iPhone15/iPhone 15 Pro Max.webp"
+import iPhone15ProMax1 from "./smartphones/iPhone/iPhone15/iPhone 15 Pro Max B.webp"
+import iPhone15ProMax2 from "./smartphones/iPhone/iPhone15/iPhone 15 Pro Max FB.webp"
+import iPhone14Plus from "./smartphones/iPhone/iPhone14/iPhone 14 Plus.webp"
+import iPhone14Plus1 from "./smartphones/iPhone/iPhone14/iPhone 14 Plus B.webp"
+import iPhone14Plus2 from "./smartphones/iPhone/iPhone14/iPhone 14 Plus FB.webp"
+import OnePlus10T from "./smartphones/OnePlus/OnePlus10T/ONEPLUS 10T.webp"
+import OnePlus10T1 from "./smartphones/OnePlus/OnePlus10T/ONEPLUS 10T B.webp"
+import OnePlus10T2 from "./smartphones/OnePlus/OnePlus10T/ONEPLUS 10T FB.webp"
+import OnePlus11 from "./smartphones/OnePlus/OnePlus11/ONEPLUS 11.webp"
+import OnePlus111 from "./smartphones/OnePlus/OnePlus11/ONEPLUS 11 B.webp"
+import OnePlus112 from "./smartphones/OnePlus/OnePlus11/ONEPLUS 11 FB.webp"
+import OppoFindX5 from "./smartphones/Oppo/Find X5/OPPO Find X5.webp"
+import OppoFindX51 from "./smartphones/Oppo/Find X5/OPPO Find X5 B.webp"
+import OppoFindX52 from "./smartphones/Oppo/Find X5/OPPO Find X5 FB.webp"
+import OppoReno10 from "./smartphones/Oppo/Reno 10/OPPO Reno10.webp"
+import OppoReno101 from "./smartphones/Oppo/Reno 10/OPPO Reno10 B.webp"
+import OppoReno102 from "./smartphones/Oppo/Reno 10/OPPO Reno10 FB.webp"
+import SamsungGalaxyS22 from "./smartphones/Samsung/Samsung S22/SAMSUNG Galaxy S22.webp"
+import SamsungGalaxyS221 from "./smartphones/Samsung/Samsung S22/SAMSUNG Galaxy S22 B.webp"
+import SamsungGalaxyS222 from "./smartphones/Samsung/Samsung S22/SAMSUNG Galaxy S22 FB.webp"
+import SamsungGalaxyS23Plus from "./smartphones/Samsung/Samsung S23 Plus/SAMSUNG Galaxy S23 Plus.webp"
+import SamsungGalaxyS23Plus1 from "./smartphones/Samsung/Samsung S23 Plus/SAMSUNG Galaxy S23 Plus B.webp"
+import SamsungGalaxyS23Plus2 from "./smartphones/Samsung/Samsung S23 Plus/SAMSUNG Galaxy S23 Plus FB.webp"
+import SamsungGalaxyS24Ultra from "./smartphones/Samsung/Samsung S24 Ultra/SAMSUNG Galaxy S24 Ultra.webp"
+import SamsungGalaxyS24Ultra1 from "./smartphones/Samsung/Samsung S24 Ultra/SAMSUNG Galaxy S24 Ultra B.webp"
+import SamsungGalaxyS24Ultra2 from "./smartphones/Samsung/Samsung S24 Ultra/SAMSUNG Galaxy S24 Ultra FB.webp"
+import Xiaomi12 from "./smartphones/Xiaomi/Xiaomi 12/Xiaomi 12.webp"
+import Xiaomi121 from "./smartphones/Xiaomi/Xiaomi 12/Xiaomi 12 B.webp"
+import Xiaomi122 from "./smartphones/Xiaomi/Xiaomi 12/Xiaomi 12 FB.webp"
+import Xiaomi13T from "./smartphones/Xiaomi/Xiaomi 13/Xiaomi 13T.webp"
+import Xiaomi13T1 from "./smartphones/Xiaomi/Xiaomi 13/Xiaomi 13T B.webp"
+import Xiaomi13T2 from "./smartphones/Xiaomi/Xiaomi 13/Xiaomi 13T FB.webp"
+import XiaomiPoco from "./smartphones/Xiaomi/Xiaomi Poco F4 GT/Xiaomi POCO.webp"
+import XiaomiPoco1 from "./smartphones/Xiaomi/Xiaomi Poco F4 GT/Xiaomi POCO B.webp"
+import XiaomiPoco2 from "./smartphones/Xiaomi/Xiaomi Poco F4 GT/Xiaomi POCO FB.webp"
 
-export {logo}
+export {
+  logo,
+  HuaweiMate50Pro,
+  HuaweiMate50Pro1,
+  HuaweiMate50Pro2,
+  HuaweiNova11Pro,
+  HuaweiNova11Pro1,
+  HuaweiNova11Pro2,
+  HuaweiP60Pro,
+  HuaweiP60Pro1,
+  HuaweiP60Pro2,
+  iPhone15ProMax,
+  iPhone15ProMax1,
+  iPhone15ProMax2,
+  iPhone14Plus,
+  iPhone14Plus1,
+  iPhone14Plus2,
+  OnePlus10T,
+  OnePlus10T1,
+  OnePlus10T2,
+  OnePlus11,
+  OnePlus111,
+  OnePlus112,
+  OppoFindX5,
+  OppoFindX51,
+  OppoFindX52,
+  OppoReno10,
+  OppoReno101,
+  OppoReno102,
+  SamsungGalaxyS22,
+  SamsungGalaxyS221,
+  SamsungGalaxyS222,
+  SamsungGalaxyS23Plus,
+  SamsungGalaxyS23Plus1,
+  SamsungGalaxyS23Plus2,
+  SamsungGalaxyS24Ultra,
+  SamsungGalaxyS24Ultra1,
+  SamsungGalaxyS24Ultra2,
+  Xiaomi12,
+  Xiaomi121,
+  Xiaomi122,
+  Xiaomi13T,
+  Xiaomi13T1,
+  Xiaomi13T2,
+  XiaomiPoco,
+  XiaomiPoco1,
+  XiaomiPoco2,
+}
