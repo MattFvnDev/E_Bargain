900a9c441a6005dee692f0d94c0f05f88bc27d9f
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-02-19 19:17:05 +0100

    BestProducts UI text color adjustment

--- src/components/BestProducts.jsx
+++ src/components/BestProducts.jsx
@@ -8,7 +8,7 @@ const BestProducts = () => {
   )
   return (
     <div className="py-32 max-w-screen-xl mx-auto lg:max-w-screen-2xl">
-      <h2 className="text-2xl font-semibold mb-16 px-6">Our best products</h2>
+      <h2 className="text-2xl font-semibold mb-16 px-6 text-red-800">Our best products</h2>
       <div className="mx-auto max-w-screen-xl px-6 lg:max-w-screen-2xl">
         <div className="grid gap-10 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
           {filteredSmartPhones.map((filteredSmartPhone) => (
