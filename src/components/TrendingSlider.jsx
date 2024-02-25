c45a0b7d84e81666fe1859980efe564e3127cac0
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-02-25 19:27:30 +0100

    Creating a feature/functionality for  a TrendingSlider component

--- src/components/TrendingSlider.jsx
+++ src/components/TrendingSlider.jsx
@@ -1,28 +1,41 @@
 import React from "react"
-// import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from "react-icons/md"
 import { BsArrowLeftSquareFill, BsArrowRightSquareFill } from "react-icons/bs"
 import { smartPhones } from "../constants"
 import { TrendingPhone } from "./"
 
 const TrendingSlider = () => {
   const trendingPhones = smartPhones.filter(
-    (trendingPhone) => trendingPhone.id >= 8
+    (trendingPhone) => trendingPhone.id >= 7
   )
+
+  const slideBackward = () => {
+    let slider = document.getElementById("slider")
+    slider.scrollLeft = slider.scrollLeft - 304
+  }
+
+  const slideForward = () => {
+    let slider = document.getElementById("slider")
+    slider.scrollLeft = slider.scrollLeft + 304
+  }
+
   return (
     <div className="pb-40">
       <div className="mx-auto py-0 px-6 max-w-screen-2xl">
         <div className="flex justify-between">
-          <h3 className="font-semibold mb-16 text-2xl">Trending Now</h3>
+          <h3 className="font-semibold mb-12 text-2xl text-lime-600 underline">Trending Now</h3>
           <div className="flex gap-2 items-start xl:gap-4">
-            <button>
+            <button title="scroll left" onClick={slideBackward}>
               <BsArrowLeftSquareFill className="w-10 h-10 hover:text-red-800" />
             </button>
-            <button>
+            <button title="scroll right" onClick={slideForward}>
               <BsArrowRightSquareFill className="w-10 h-10 hover:text-red-800" />
             </button>
           </div>
         </div>
-        <div className="flex gap-7 overflow-x-scroll overflow-y-hidden whitespace-nowrap scroll-smooth relative">
+        <div
+          className="flex gap-11 overflow-x-scroll overflow-y-hidden whitespace-nowrap scroll-smooth relative"
+          id="slider"
+        >
           {trendingPhones.map((trendingPhone) => (
             <TrendingPhone key={trendingPhone.id} {...trendingPhone} />
           ))}
