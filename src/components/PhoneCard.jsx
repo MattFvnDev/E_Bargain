4fd4e284aea32b64ab46ba9682a08f7f67f1884c
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-02-28 19:17:16 +0100

    Pulling data from Redux store to Products Page and PhoneCard

--- src/components/PhoneCard.jsx
+++ src/components/PhoneCard.jsx
@@ -2,36 +2,34 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { Rating } from "../components"
 
-const PhoneCard = ({ id, img, brand, model, price, rating, ratingLeft }) => {
+const PhoneCard = ({ phone }) => {
   return (
-    <Link
-      onClick={() => window.top(0.0)}
-      to={`/products/phone/${id}`}
-      className="border-2 rounded-md group overflow-hidden hover:border-gray-500 duration-300 ease-in"
-    >
-      <div className="relative w-full">
-        <img
-          src={img}
-          alt={`An Image of ${model}`}
-          className="object-contain py-6"
-        />
-      </div>
+    <div className="border-2 rounded-md group overflow-hidden hover:border-gray-500 duration-300 ease-in">
+      <Link onClick={() => window.top(0.0)} to={`/products/phone/${phone.id}`}>
+        <div className="relative w-full">
+          <img
+            src={phone.img}
+            alt={`An Image of ${phone.model}`}
+            className="object-contain py-6"
+          />
+        </div>
+      </Link>
       <div className="p-6 bg-white">
         <p className="text-lg font-semibold">
-          {brand} {model}
+          {phone.brand} {phone.model}
         </p>
-        <Rating rating={rating} ratingLeft={ratingLeft} />
+        <Rating rating={phone.rating} ratingLeft={phone.ratingLeft} />
         <div className="mt-4 flex items-center justify-between space-x-2">
           <div>
             <p className="text-gray-600">Price</p>
-            <p className="text-lg font-semibold">${price}.00</p>
+            <p className="text-lg font-semibold">${phone.price}.00</p>
           </div>
-          <button className="border rounded-lg py-1 px-4 font-semibold hover:bg-lime-500 hover:border-lime-500 focus:ring-4 focus: ring-opacity-50 focus:ring-lime-500 hover:text-white duration-300 ease-in-out">
-            Add to Cart
+          <button className="bg-lime-500 border rounded-lg py-1 px-4 font-semibold hover:bg-lime-600 hover:lime-red-600 focus:ring-4 focus: ring-opacity-50 focus:ring-lime-700 duration-300 ease-in-out text-white">
+            Add To Cart
           </button>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
 
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,14 @@
 import React from "react"
-import { smartPhones } from "../constants"
 import { PhoneCard } from "../components"
+import { useSelector } from "react-redux"
 
 const Products = () => {
+  const smartPhones = useSelector((state) => state.products.smartPhones)
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
       <div className="grid gap-10 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
         {smartPhones.map((phone) => (
-          <PhoneCard key={phone.id} {...phone} phone={phone}  />
+          <PhoneCard phone={phone} key={phone.id} />
         ))}
       </div>
     </div>
