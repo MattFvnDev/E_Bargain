47de7460aada261692903f4baf7a9d93d2fdf7fd
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-02-29 18:36:51 +0100

    Pulling data from Redux store to BestProduct and it's children

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,24 +1,35 @@
 import React from "react"
+import { BsFillCartPlusFill } from "react-icons/bs"
 import { Link } from "react-router-dom"
 
-const Product = ({ id, otherImgs, brand, model, price }) => {
+const Product = ({ filteredSmartPhone }) => {
   return (
     <div className="border-2 hover:border-gray-500 duration-300 ease-in-out">
-      <Link onClick={() => window.top(0.0)} to={`/products/phone/${id}`}>
+      <Link
+        onClick={() => window.top(0.0)}
+        to={`/products/phone/${filteredSmartPhone.id}`}
+      >
         <div className="p-4 w-full">
           <img
-            src={otherImgs[0]}
-            alt={`An Image of ${brand} ${model}`}
+            src={filteredSmartPhone.otherImgs[0]}
+            alt={`An Image of ${filteredSmartPhone.brand} ${filteredSmartPhone.model}`}
             className="object-contain"
           />
         </div>
-        <div className="p-4 bg-white">
-          <p className="text-lg text-lime-600 font-semibold">
-            {brand} {model}
-          </p>
-          <p className="mt-1 text-xl font-semibold text-red-800">
-            ${price}.00
-          </p>
+        <div className="flex items-center bg-white justify-between p-4 ">
+          <div className="">
+            <p className="text-lg text-lime-600 font-semibold">
+              {filteredSmartPhone.brand} {filteredSmartPhone.model}
+            </p>
+            <p className="mt-1 text-xl font-semibold text-red-800">
+              ${filteredSmartPhone.price}.00
+            </p>
+          </div>
+          <div className="flex gap-2 self-end">
+            <button className="text-yellow-500 hover:text-red-800 ">
+              <BsFillCartPlusFill className="w-7 h-7" />
+            </button>
+          </div>
         </div>
       </Link>
     </div>
