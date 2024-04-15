42620b1ae55db7b51321e4e6ed29a0c7a2742d6d
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-04-15 19:43:01 +0200

    Extracting Navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react"
-import { Logo } from "./"
+import { Logo, Navbar } from "./"
+
 
 const Header = () => (
   <header className="sticky top-0, bg-white z-20 shadow">
     <div className="container mx-auto p-6 flex justify-between">
       <Logo />
+      <Navbar/>
     </div>
   </header>
 )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react"
+import { NavLink } from "react-router-dom"
+import { HiMiniShoppingCart } from "react-icons/hi2"
+import { BsBoxes } from "react-icons/bs"
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+const Navbar = () => (
+  <div className="flex flex-row space-x-4 md:space-x-6">
+    <NavLink
+      className="flex items-center text-gray-600 hover:text-red-800 group"
+      to="products"
+    >
+      <span className="hidden text-2xl font-bold md:inline-block">
+        Products
+      </span>
+      <BsBoxes className="w-7 h-7 flex-shrink-0 md:hidden group-hover:scale-110" />
+    </NavLink>
+    <NavLink
+      to="cart"
+      className="flex items-center space-x-2 text-gray-600 hover:text-red-800"
+    >
+      <div className="relative">
+        <HiMiniShoppingCart className="w-7 h-7 flex-shrink-0" />
+      </div>
+      <p className="text-xl">
+        $0.00 <span className="text-sm text-gray-500">(0)</span>
+      </p>
+    </NavLink>
+  </div>
+)
 
 export default Navbar
