82cd9844d1d156106e2bb48a1b03325c72636013
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-04-16 15:28:53 +0200

    Footer Date adjustment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,12 @@ import {
 import { Link } from "react-router-dom"
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
   return (
     <footer className="border-t border-gray-100 p-6 text-center container mx-auto">
       <div className="flex flex-col-reverse items-center justify-center sm:flex-row sm:justify-between">
         <p className="text-sm font-medium text-gray-500 xl:text-lg">
-          &copy; {new Date().getFullYear()} Bargain. All rights reserved.
+          &copy; {currentYear} Bargain. All rights reserved.
         </p>
         <div className="flex flex-row items-center justify-center space-x-5 py-3 ">
           <Link
