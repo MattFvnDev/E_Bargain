6149aecb1c1341f1c7ea8aa0c760660cb1649d08
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-04-18 23:32:05 +0200

    Cart component UI

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from "react"
 
 const Cart = () => {
+  
   return (
-    <div>Cart</div>
+    <div>
+      <main className="mx-auto max-w-xl md:max-w-2xl px-6 pb-16 pt-16 lg:max-w-5xl xl:max-w-7xl lg:px-8">
+        <h1 className="font-semibold text-4xl xl:text-3xl">Shopping Cart</h1>
+        <form
+          value=""
+          action=""
+          className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16"
+        >
+          <section className="lg:col-span-7">
+            <h2 className="sr-only">Products in your shopping cart</h2>
+            
+          </section>
+
+          <section className="mt-16 rounded-lg border-2 border-gray-300 bg-gray-50 px-4 py-6 shadow-md sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+            <h2 className="text-lg font-medium">Order summary</h2>
+            <dl className="mt-6 space-y-4">
+              <div className="flex items-center justify-between">
+                <dt className="text-sm">Subtotal</dt>
+                <dd className="text-sm font-medium">$0.00&nbsp;</dd>
+              </div>
+              <div className="flex items-center justify-between border-t border-gray-300 pt-4">
+                <dt className="flex items-center text-sm">
+                  <span>Shipping estimate</span>
+                </dt>
+                <dd className="text-sm font-medium">$0.00&nbsp;</dd>
+              </div>
+              <div className="flex items-center justify-between border-t border-gray-300 pt-4">
+                <dt className="text-base font-medium">Order total</dt>
+                <dd className="text-base font-medium">$0.00&nbsp;</dd>
+              </div>
+            </dl>
+            <div className="mt-6">
+              <button className="text-white inline-flex items-center justify-center rounded-md text-md font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 py-2 px-4 w-full bg-gray-700 hover:bg-red-800 ease-in-out duration-300">
+                Checkout
+              </button>
+            </div>
+          </section>
+        </form>
+      </main>
+    </div>
   )
 }
 
