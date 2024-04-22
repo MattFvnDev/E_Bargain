70447fdf82fe7ba277329b6e5f62a90b24ae9769
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-04-22 22:17:19 +0200

    TrendingSlider UI adjustments

--- src/components/TrendingSlider.jsx
+++ src/components/TrendingSlider.jsx
@@ -4,7 +4,7 @@ import { TrendingPhone } from "./"
 import { useSelector } from "react-redux"
 
 const TrendingSlider = () => {
-  const {smartPhones} = useSelector((state) => state.products)
+  const { smartPhones } = useSelector((state) => state.products)
   const trendingPhones = smartPhones.filter(
     (trendingPhone) => trendingPhone.id >= 7
   )
@@ -22,17 +22,9 @@ const TrendingSlider = () => {
   return (
     <div className="pb-40">
       <div className="mx-auto py-0 px-6 max-w-screen-2xl">
-        <div className="flex justify-between">
-          <h3 className="font-semibold mb-12 text-2xl text-lime-600 underline">Trending Now</h3>
-          <div className="flex gap-2 items-start xl:gap-4">
-            <button title="scroll left" onClick={slideBackward}>
-              <BsArrowLeftSquareFill className="w-10 h-10 hover:text-red-800" />
-            </button>
-            <button title="scroll right" onClick={slideForward}>
-              <BsArrowRightSquareFill className="w-10 h-10 hover:text-red-800" />
-            </button>
-          </div>
-        </div>
+        <h3 className="font-semibold mb-6 text-2xl text-lime-600 underline">
+          Trending Now
+        </h3>
         <div
           className="flex gap-11 overflow-x-scroll overflow-y-hidden whitespace-nowrap scroll-smooth relative"
           id="slider"
@@ -40,6 +32,14 @@ const TrendingSlider = () => {
           {trendingPhones.map((trendingPhone) => (
             <TrendingPhone key={trendingPhone.id} {...trendingPhone} />
           ))}
+        </div>
+        <div className="flex gap-2 items-start xl:gap-4 mt-6 justify-end mr-2">
+          <button title="scroll left" type="button" onClick={slideBackward}>
+            <BsArrowLeftSquareFill className="w-10 h-10 hover:text-red-800" />
+          </button>
+          <button title="scroll right" type="button" onClick={slideForward}>
+            <BsArrowRightSquareFill className="w-10 h-10 hover:text-red-800" />
+          </button>
         </div>
       </div>
     </div>
