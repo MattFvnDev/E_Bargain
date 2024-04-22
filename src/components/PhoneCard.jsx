2454976fed428874bac6b3dfb0e9eb737c3fca34
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-04-22 20:11:11 +0200

    PhoneDetails and its children code refactoring

--- src/components/PhoneCard.jsx
+++ src/components/PhoneCard.jsx
@@ -4,28 +4,37 @@ import { Rating } from "../components"
 import { addToCart } from "../features/cartSlice"
 import { useDispatch } from "react-redux"
 
-const PhoneCard = ({ phone }) => {
+const PhoneCard = ({
+  id,
+  img,
+  model,
+  brand,
+  rating,
+  ratingLeft,
+  price,
+  phone,
+}) => {
   const dispatch = useDispatch()
   return (
     <div className="border-2 rounded-md group overflow-hidden hover:border-gray-500 duration-300 ease-in">
-      <Link onClick={() => window.top(0.0)} to={`/products/phone/${phone.id}`}>
+      <Link onClick={() => window.top(0.0)} to={`/products/phone/${id}`}>
         <div className="relative w-full">
           <img
-            src={phone.img}
-            alt={`An Image of ${phone.model}`}
+            src={img}
+            alt={`An Image of ${model}`}
             className="object-contain py-6"
           />
         </div>
       </Link>
       <div className="p-6 bg-white">
         <p className="text-lg font-semibold">
-          {phone.brand} {phone.model}
+          {brand} {model}
         </p>
-        <Rating rating={phone.rating} ratingLeft={phone.ratingLeft} />
+        <Rating rating={rating} ratingLeft={ratingLeft} />
         <div className="mt-4 flex items-center justify-between space-x-2">
           <div>
             <p className="text-gray-600">Price</p>
-            <p className="text-lg font-semibold">${phone.price.toFixed(2)}</p>
+            <p className="text-lg font-semibold">${price.toFixed(2)}</p>
           </div>
           <button
             onClick={() => dispatch(addToCart(phone))}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,11 +4,12 @@ import { useSelector } from "react-redux"
 
 const Products = () => {
   const smartPhones = useSelector((state) => state.products.smartPhones)
+
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
       <div className="grid gap-10 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
         {smartPhones.map((phone) => (
-          <PhoneCard phone={phone} key={phone.id} />
+          <PhoneCard key={phone.id} phone={phone} {...phone} />
         ))}
       </div>
     </div>
