79b4730ea2b86eb8225f37239bc0e0f6261134ea
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-04-23 17:33:32 +0200

    BestProducts filter change

--- src/components/BestProducts.jsx
+++ src/components/BestProducts.jsx
@@ -5,11 +5,13 @@ import { useSelector } from "react-redux"
 const BestProducts = () => {
   const smartPhones = useSelector((state) => state.products.smartPhones)
   const filteredSmartPhones = smartPhones.filter(
-    (filteredSmartPhone) => filteredSmartPhone.id <= 8
+    (filteredSmartPhone) => filteredSmartPhone.price >= 999
   )
   return (
     <div className="py-32 max-w-screen-xl mx-auto lg:max-w-screen-2xl">
-      <h2 className="text-2xl font-semibold mb-12 px-6 text-red-800 underline">Our best products</h2>
+      <h2 className="text-2xl font-semibold mb-12 px-6 text-red-800 underline">
+        Our best products
+      </h2>
       <div className="mx-auto max-w-screen-xl px-6 lg:max-w-screen-2xl">
         <div className="grid gap-10 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
           {filteredSmartPhones.map((filteredSmartPhone) => (
