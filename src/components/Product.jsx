5d0f6064108c4c850b6ab29ce8b3b7f5ae2f16f1
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-04-25 17:34:04 +0200

    Adding function and conditional rendering to BestProducts and its child

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,8 @@
 import React from "react"
-import { BsFillCartCheckFill } from "react-icons/bs"
+import { BsFillCartCheckFill, BsCartXFill } from "react-icons/bs"
 import { Link } from "react-router-dom"
-import { addToCart } from "../features/cartSlice"
-import { useDispatch } from "react-redux"
+import { addToCart, removeFromCart } from "../features/cartSlice"
+import { useDispatch, useSelector } from "react-redux"
 
 const Product = ({
   img,
@@ -14,6 +14,7 @@ const Product = ({
   memoryCapacity,
   quantity,
 }) => {
+  const { cart } = useSelector((state) => state.cart)
   const dispatch = useDispatch()
   return (
     <div className="border-2 border-gray-300 hover:border-gray-500 duration-300 ease-in-out">
@@ -36,25 +37,34 @@ const Product = ({
           </p>
         </div>
         <div className="flex gap-2 self-end">
-          <button
-            onClick={() =>
-              dispatch(
-                addToCart({
-                  brand,
-                  model,
-                  price,
-                  img,
-                  quantity,
-                  memoryCapacity,
-                  id,
-                })
-              )
-            }
-            type="button"
-            className="text-yellow-500 hover:text-red-800 "
-          >
-            <BsFillCartCheckFill className="w-8 h-8" />
-          </button>
+          {cart.findIndex((item) => item.id === id) < 0 ? (
+            <button
+              onClick={() =>
+                dispatch(
+                  addToCart({
+                    brand,
+                    model,
+                    price,
+                    img,
+                    quantity,
+                    memoryCapacity,
+                    id,
+                  })
+                )
+              }
+              type="button"
+              className="text-yellow-500 hover:text-red-800 "
+            >
+              <BsFillCartCheckFill className="w-8 h-8" />
+            </button>
+          ) : (
+            <button
+              onClick={() => dispatch(removeFromCart(id))}
+              className="text-gray-500 hover:text-red-800 "
+            >
+              <BsCartXFill className="w-8 h-8" />
+            </button>
+          )}
         </div>
       </div>
     </div>
