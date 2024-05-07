b268a63f431c256c5de6ea1b545a633885b2265d
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-05-07 18:33:48 +0200

    Header component width adjustment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Logo, Navbar } from "./"
 
 const Header = () => (
   <header className="sticky top-0, bg-white z-20 shadow">
-    <div className="container mx-auto p-6 flex justify-between">
+    <div className="container mx-auto p-6 flex justify-between max-w-screen-lg lg:max-w-screen-2xl">
       <Logo />
       <Navbar/>
     </div>
