6bd6ffb5f7d3e9b4c4fded471653b99ea85ca6ee
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-05-14 21:08:18 +0200

    Sticky Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react"
 import { Logo, Navbar } from "./"
 
-const Header = () => (
-  <header className="sticky top-0, bg-white z-20 shadow">
-    <div className="container mx-auto p-6 flex justify-between max-w-screen-lg lg:max-w-screen-2xl">
-      <Logo />
-      <Navbar/>
-    </div>
-  </header>
-)
+const Header = () => {
+  const [sticky, setSticky] = useState(false)
+  const stickyHeader = () => {
+    if (window.scrollY >= 90) {
+      setSticky(true)
+    } else {
+      setSticky(false)
+    }
+  }
+  window.addEventListener("scroll", stickyHeader)
+
+  return (
+    <header
+      className={
+        sticky
+          ? "sticky top-0 bg-white z-20 shadow-md"
+          : "sticky top-0, bg-white z-20 shadow"
+      }
+    >
+      <div className="container mx-auto p-6 flex justify-between max-w-screen-lg lg:max-w-screen-2xl">
+        <Logo />
+        <Navbar />
+      </div>
+    </header>
+  )
+}
 
 export default Header
